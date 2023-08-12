f08e18fc79faae2ea8dcf71dcd874aefd8259eb1
ashutoshsameer <sameerashutosh0201@gmail.com>
2023-08-12 13:59:27 -0400

    Interpreter Code

--- 03_06/begin/interpreter.py
+++ 03_06/begin/interpreter.py
@@ -1,16 +1,19 @@
-
+from abc import ABC, abstractmethod
 
 class AbstractExpression():
 
-	
+	@abstractmethod
+	def interpret(self):
+		pass
 
 class NonterminalExpression(AbstractExpression):
 
 	def __init__(self, expression):
+		self._expression = expression
 			
-
 	def interpret(self):
 		print("Non-terminal expression being interpreted ...")
+		self._expression.interpret()
 		
 
 class TerminalExpression(AbstractExpression):
@@ -19,8 +22,8 @@ class TerminalExpression(AbstractExpression):
 		print("Terminal expression being interpreted ...")
 
 def main():
-	
-	
+	ast = NonterminalExpression(NonterminalExpression(TerminalExpression()))
+	ast.interpret()
 
 if __name__ == "__main__":
 	main()
